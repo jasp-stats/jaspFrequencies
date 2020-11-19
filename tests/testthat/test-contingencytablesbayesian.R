13910f0c985628ba462199a555573443438396bb
TimKDJ <tim_jong@hotmail.com>
2020-11-19 12:10:46 +0100

    Remove trailing whitespace from contingency files

--- tests/testthat/test-contingencytablesbayesian.R
+++ tests/testthat/test-contingencytablesbayesian.R
@@ -20,9 +20,9 @@ test_that("Main table results match", {
   table <- results[["results"]][["container1"]][["collection"]][["container1_crossTabMain"]][["data"]]
   jaspTools::expect_equal_tables(table,
     list("TRUE", 320, 495, "control", "f", 815, 334, 202, "experimental",
-         "f", 536, "TRUE", 654, 697, "Total", "f", 1351, 
+         "f", 536, "TRUE", 654, 697, "Total", "f", 1351,
          "TRUE", 253, 182, "control", "m", 435, 494, 270, "experimental",
-         "m", 764, "TRUE", 747, 452, "Total", "m", 1199, 
+         "m", 764, "TRUE", 747, 452, "Total", "m", 1199,
          "TRUE", 573, 677, "control", "Total", 1250, 828, 472,
          "experimental", "Total", 1300, "TRUE", 1401, 1149,
          "Total", "Total", 2550)
@@ -79,30 +79,30 @@ test_that("Bayesian Contingency Tables Tests table results match - different hyp
   options$rows <- "facExperim"
   options$columns <- "contBinom"
   options$priorConcentration <- 1.5
-  
+
   samplingModels <- c("poisson", "jointMultinomial", "independentMultinomialRowsFixed",
                       "independentMultinomialColumnsFixed")
   hypotheses <- c("groupsNotEqual", "groupOneGreater", "groupTwoGreater")
-  
+
   refTables <- list(
     poisson = list(
       groupsNotEqual  = list("BF<unicode><unicode> Poisson", "N", 100, 0.523118843924781),
-      groupOneGreater = list("BF<unicode><unicode> Poisson", "N", 100, 0.224941102887656),             
+      groupOneGreater = list("BF<unicode><unicode> Poisson", "N", 100, 0.224941102887656),
       groupTwoGreater = list("BF<unicode><unicode> Poisson", "N", 100, 0.818576366973497)),
     jointMultinomial = list(
       groupsNotEqual  = list("BF<unicode><unicode> joint multinomial", "N", 100, 0.440084106793853),
-      groupOneGreater = list("BF<unicode><unicode> joint multinomial", "N", 100, 0.181490685641785),             
+      groupOneGreater = list("BF<unicode><unicode> joint multinomial", "N", 100, 0.181490685641785),
       groupTwoGreater = list("BF<unicode><unicode> joint multinomial", "N", 100, 0.683978718779006)),
     independentMultinomialRowsFixed = list(
       groupsNotEqual  = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.35545254779504),
-      groupOneGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.149361160583476),             
+      groupOneGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.149361160583476),
       groupTwoGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.560761939401455)),
     independentMultinomialColumnsFixed = list(
       groupsNotEqual  = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.364069579256729),
-      groupOneGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.153564548530488),             
+      groupOneGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.153564548530488),
       groupTwoGreater = list("BF<unicode><unicode> independent multinomial", "N", 100, 0.571734867264767))
   )
-  
+
   for (samplingModel in samplingModels) {
     options$samplingModel <- samplingModel
     for(hypothesis in hypotheses) {
