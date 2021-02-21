1f9116e213f3a4837504634d247c95032ced7a75
Alexander Ly <Alexander.Ly.NL@gmail.com>
2021-02-21 14:36:58 +0100

    try multinomial on a whim

--- tests/testthat/test-multinomialtest.R
+++ tests/testthat/test-multinomialtest.R
@@ -1,5 +1,41 @@
 context("Multinomial Test")
 
+# https://jasp-stats.github.io/jasp-verification-project/frequencies.html#multinomial-test-chi-square-goodness-of-fit-test
+test_that("Main table results match R, SPSS, SAS", {
+  options <- jaspTools::analysisOptions("MultinomialTest")
+  
+  options$counts <- "Count"
+  options$factor <- "Color"
+  options$hypothesis <- "Expected"
+  options$descriptivesPlot <- TRUE
+  options$tableWidget <- list(
+    list(
+      levels = list("Black", "Red", "White"),
+      name = "H1",
+      values = list(4, 3, 3)
+    )
+  )
+  
+  results <- jaspTools::runAnalysis("MultinomialTest",
+                                    "MNT.csv", options)
+  
+  # Main table
+  resultTable <- results$results$chisqTable$data
+  
+  refTable <- jaspTools:::collapseTestTable(
+    list(list(case = "H\u2080 (a)",
+              chisquare = 2.33333333333333,
+              df = 2,
+              p = 0.311403223914598))
+  )
+  
+  jaspTools::expect_equal_tables("test"= resultTable, "ref"=refTable)
+  
+  # Descriptive Plot
+  resultPlot <- results[["state"]][["figures"]][[1]][["obj"]]
+  # jaspTools::expect_equal_plots(resultPlot, "descriptives-1", dir="MultinomialTest")
+})
+
 test_that("Main table results match", {
   options <- jaspTools::analysisOptions("MultinomialTest")
   options$VovkSellkeMPR <- TRUE
