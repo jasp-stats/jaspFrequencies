5411e726f6be8f726cddcba35a01e6dfbcace6c9
bruno boutin <b.boutin@uva.nl>
2021-11-02 17:11:45 +0100

    Workaround for gettextf

--- R/binomialtest.R
+++ R/binomialtest.R
@@ -15,6 +15,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# This is a temporary fix
+# TODO: remove it when R will solve this problem!
+gettextf <- function(fmt, ..., domain = NULL)  {
+  return(sprintf(gettext(fmt, domain = domain), ...))
+}
+
 BinomialTest <- function(jaspResults, dataset = NULL, options, ...) {
   ready <- length(options$variables) > 0
 
